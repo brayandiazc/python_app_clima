c48b1153b46e495a8ac4841320c4862e52117878
Brayan Diaz C <brayandiazc@gmail.com>
2024-09-06 17:15:12 -0500

    feat Etapa 4: Manejo de Arreglos y Persistencia de Datos

---
+++ etapa-4/main.py
@@ -0,0 +1,27 @@
+# Etapa 3: Creación y Uso de Métodos
+
+# Importar métodos
+from clima_metodos import mostrar_recomendacion, guardar_en_archivo
+
+# Bienvenida e inicio del programa
+print("¡Bienvenido al sistema de clima!")
+consultas = []
+continuar = True
+
+while continuar:
+    # Ingreso de ciudad y temperatura
+    ciudad = input("\nIngrese el nombre de la ciudad: ").strip()
+    temperatura = int(input("Ingrese la temperatura actual: ").strip())
+
+    # Llamar al método para mostrar la recomendación
+    mostrar_recomendacion(temperatura)
+
+    # Llamar al método para mostrar la recomendación
+    consultas.append({"ciudad": ciudad, "temperatura": temperatura})
+
+    # Llamar al método para guardar los datos en el archivo CSV
+    guardar_en_archivo(ciudad, temperatura)
+
+    # Pregunta si desea continuar
+    respuesta = input("¿Desea consultar otra temperatura? (si/no): ").strip().lower()
+    continuar = respuesta == "si"
